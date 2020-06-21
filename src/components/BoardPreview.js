ab262f755cee0fcbb4266b368cab43394f52f175
JudyLime <56984748+judylime@users.noreply.github.com>
2020-06-21 14:14:01 -0700

    Using withRouter and push

--- src/components/BoardPreview.js
+++ src/components/BoardPreview.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Board from './Board';
 import PropTypes from 'prop-types';
+import { withRouter } from "react-router-dom";
 
 class BoardPreview extends React.Component {
+  goToBoard = () => {
+    const boardId = this.props.board.id
+    this.props.histery.push ({
+      pathname:`/board/${boardId}`
+    })
+  }
   render ( ) {
     return (
-      <p>{this.props.board.title} </p>
+      <ul
+      className="board-preview-item" 
+      onClick={this.goToBoard}
+      style={{backgroundColor:this.props.board.background}}
+      >
+        <li>{this.props.board.title} </li>
+      </ul>
     )
   }
 }
@@ -13,4 +26,4 @@ class BoardPreview extends React.Component {
 BoardPreview.propTypes ={
   board: PropTypes.object.isRequired
 }
-export default BoardPreview
+export default withRouter(BoardPreview)
